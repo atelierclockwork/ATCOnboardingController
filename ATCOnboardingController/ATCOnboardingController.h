53c888f82b09669ac5bd5107819db2c707518d27
Michael Skiba <mike.skiba@me.com>
2014-12-19 11:26:57 -0500

    Initial Commit

---
+++ ATCOnboardingController/ATCOnboardingController.h
@@ -0,0 +1,19 @@
+//
+//  ATCOnboardingController.h
+//  ATCOnboardingController
+//
+//  Created by Michael Skiba on 12/19/14.
+//  Copyright (c) 2014 Michael Skiba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ATCOnboardingController.
+FOUNDATION_EXPORT double ATCOnboardingControllerVersionNumber;
+
+//! Project version string for ATCOnboardingController.
+FOUNDATION_EXPORT const unsigned char ATCOnboardingControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ATCOnboardingController/PublicHeader.h>
+
+
